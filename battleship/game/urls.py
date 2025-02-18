04007d8fb3a45817e8845edbfbebaa0da3836c18
Cyber-Kaeh <153845457+Cyber-Kaeh@users.noreply.github.com>
2025-02-18 14:47:38 -0500

    trying to fix matchmaking. so far everything kind of works but a enw game is created every time.

--- battleship/game/urls.py
+++ battleship/game/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import signup_view, matchmaking_view
+from .views import signup_view, matchmaking_view, game_room_view
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
     path('matchmaking/', matchmaking_view, name='matchmaking'),
+    path("game/<int:game_id>/", game_room_view, name="game_room"),
 ]
--- battleship/game/views.py
+++ battleship/game/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.urls import reverse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from .forms import SignUpForm
 from django.contrib.auth.decorators import login_required
@@ -30,7 +32,17 @@ def matchmaking_view(request):
     if open_game:
         open_game.player2 = request.user
         open_game.save()
-        return redirect('game_room', game_id=open_game.id)
+        print(f"Game {open_game.id} now has player1: {open_game.player1} and player2: {open_game.player2}")
+        # return redirect('game_room', game_id=open_game.id)
+        game_url = reverse('game_room', kwargs={'game_id': open_game.id})
+        return JsonResponse({'redirect': game_url})
     else:
         new_game = GameSession.objects.create(player1=request.user)
+        print(f"New Game {new_game.id} created with player1: {new_game.player1}")
         return render(request, 'game/matchmaking.html', {'waiting': True, 'game_id': new_game.id})
+
+
+@login_required
+def game_room_view(request, game_id):
+    game = get_object_or_404(GameSession, id=game_id)
+    return render(request, 'game/game_room.html', {'game': game})
